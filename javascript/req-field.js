a5336b5cbd194579b28dbc99a76e3d1536610694
Joonsik Kim <kimkimjunior2001@gmail.com>
2023-02-26 17:15:30 -0800

    Added required field protection for login and signup

---
+++ javascript/req-field.js
@@ -0,0 +1,30 @@
+window.onload = function(){
+    document.getElementById("mainForm").onsubmit = function(e){
+        var requiredFields = document.getElementsByClassName("required");
+        var invalid = false;
+        for(var i = 0; i < requiredFields.length; i++){
+            var fieldValue = requiredFields[i].value;
+            if(fieldValue == null || fieldValue.trim() == ""){
+                e.preventDefault();
+                requiredFields[i].classList.add("highlight");
+                invalid = true;
+            } else {
+                requiredFields[i].classList.remove("highlight");
+            }
+        }   
+        if (invalid == true){
+            alert("Please fill out the required area.");
+        }
+    };
+}
+
+
+/*if(requiredFields[i].type == "checkbox"){
+            if(!requiredFields[i].checked){
+                e.preventDefault();
+                requiredFields[i].classList.add("highlight");
+            } else {
+                requiredFields[i].classList.remove("highlight");
+            }
+        } else 
+        */
