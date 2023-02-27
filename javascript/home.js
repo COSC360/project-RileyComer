a63572b274eec4138f7f42501b955204da40ddeb
Joonsik Kim <kimkimjunior2001@gmail.com>
2023-02-26 23:33:55 -0800

    added create-post page, the image is not set yet

--- javascript/home.js
+++ javascript/home.js
@@ -11,3 +11,16 @@ function selectNew() {
     document.getElementById("top-off").style.display="inline";
     document.getElementById("top-on").style.display="none";
 }
+
+function selectText() {
+    document.getElementById("text-on").style.display="inline";
+    document.getElementById("text-off").style.display="none";
+    document.getElementById("image-off").style.display="inline";
+    document.getElementById("image-on").style.display="none";
+}
+function selectImage() {
+    document.getElementById("image-on").style.display="inline";
+    document.getElementById("image-off").style.display="none";
+    document.getElementById("text-off").style.display="inline";
+    document.getElementById("text-on").style.display="none";
+}
