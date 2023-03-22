987a05a8573177255395817a37f276a7b91d3d01
RileyComer <2001rdc@gmail.com>
2023-03-21 22:41:33 -0700

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,17 @@
 <?php
 
-//Riley's Testing
+define('DBSERVER', 'localhost'); // Database server
+define('DBUSERNAME', ''); // Database username
+define('DBPASSWORD', ''); // Database password
+define('DBNAME', ''); // Database name
 
+//Riley's Testing
+/*
 define('DBSERVER', 'localhost'); // Database server
 define('DBUSERNAME', 'rcomer'); // Database username
 define('DBPASSWORD', 'RileyDeanComer2001'); // Database password
 define('DBNAME', '360_project'); // Database name
+*/
 
 $db = mysqli_connect(DBSERVER, DBUSERNAME, DBPASSWORD, DBNAME);
  
